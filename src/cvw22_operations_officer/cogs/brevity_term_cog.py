2abe776ffe0fc73fccb0d5975e92257003a39e2c
Niklas Glienke <niklas.glienke@icloud.com>
2025-12-21 22:23:34 +0000

    bot: refactor brevity term cog
    GH: #8

--- src/cvw22_operations_officer/cogs/brevity_term_cog.py
+++ src/cvw22_operations_officer/cogs/brevity_term_cog.py
@@ -1,15 +1,14 @@
 # Copyright 2025 Niklas Glienke
 
+import datetime
 import logging
-from datetime import time
+from zoneinfo import ZoneInfo
 
 from discord import TextChannel
 from discord.ext import commands, tasks
 
 from cvw22_operations_officer.bot import DiscordBot
 from cvw22_operations_officer.models.brevity_term_model import BrevityTerm
-
-BREVITY_TERM_DIGEST_TIME = time(hour=12, minute=0)
 
 
 class BrevityTermCog(commands.Cog):
@@ -25,43 +24,69 @@ class BrevityTermCog(commands.Cog):
         self.logger = logging.getLogger(f"cvw22_operations_officer.{__name__}")
         self.bot = discord_bot
 
-    async def cog_load(self) -> None:
+    async def cog_load(self) -> None:  # pragma: no cover
         """Start 'Brevity Term Digest' task when cog is loaded."""
+        if self.brevity_term_digest.is_running():
+            return
+
         self.brevity_term_digest.start()
 
     @commands.command()
-    async def brevity_term(self, ctx: commands.Context, *args: str) -> None:
+    async def brevity_term(
+        self, ctx: commands.Context, search_term: str
+    ) -> None:
         """Coordinate all brevity term commands.
 
         Args:
             ctx: The discord context of the command.
-            *args: Action and search term of the command.
+            search_term: The term to search for.
 
         """
         if not self.bot.config["commands"]["brevity_term"]:
-            await ctx.send("Any 'brevity_term' commands have been disabled")
+            await ctx.send("The 'brevity_term' commands has been disabled.")
             return
 
-        self.logger.info(f"Execute 'brevity_term' command with {args=}")
+        self.logger.info(
+            f"Execute 'brevity_term' command with '{search_term}'."
+        )
 
-        if len(args) < 2:
-            await ctx.send(
-                "Invalid command. Usage: `!brevity_term search <search_term>`"
-            )
-            return
-        action: str = args[0]
-        search_term: str = args[1].upper()
+        await ctx.send(self._search_brevity_term(search_term))
 
-        match action:
-            case "search":
-                await ctx.send(self._search_brevity_term(search_term))
-            case _:
-                await ctx.send("Invalid action. Nothing done.")
+    def _search_brevity_term(self, search_term: str) -> str:
+        """Get matching brevity terms by a search term.
 
-    @tasks.loop(time=BREVITY_TERM_DIGEST_TIME)
+        Args:
+            search_term: The search term to search for.
+
+        Returns:
+            The formatted result of the search.
+
+        """
+        response = self.bot.brevity_term_service.get_brevity_terms_by_term(
+            search_term
+        )
+        output_message = ""
+
+        if response:
+            for brevity_term in response:
+                output_message += BrevityTermCog.format_brevity_term(
+                    brevity_term
+                )
+                output_message += "\n"
+        else:
+            output_message = f"No brevity terms found with `{search_term}`."
+
+        return output_message
+
+    @tasks.loop(minutes=1)
     async def brevity_term_digest(self):
         """Sends a brevity term at the specified time."""
         task_config = self.bot.config["tasks"]["brevity_term_digest"]
+        time_zone = ZoneInfo(task_config["timezone"])
+        time_now = datetime.datetime.now(time_zone).strftime("%H:%M")
+
+        if not time_now == task_config["time"]:
+            return
 
         if not task_config["enabled"]:
             self.logger.info("'Brevity Term Digest' task is disabled.")
@@ -95,38 +120,12 @@ class BrevityTermCog(commands.Cog):
             brevity_term: A brevity term as a BrevityTerm object.
 
         Returns:
-            The formatted brevity term in MARKDOWN as a string.
+            The formatted brevity term with MARKDOWN syntax as a string.
 
         """
         output_message = f"### Brevity Term: `{brevity_term.term}`"
 
         for description in brevity_term.description.split("&"):
             output_message += f"\n> {description.strip()}"
-
-        return output_message
-
-    def _search_brevity_term(self, search_term: str) -> str:
-        """Get matching brevity terms by a search term.
-
-        Args:
-            search_term: The search term to search for brevity terms.
-
-        Returns:
-            The formatted result of the search.
-
-        """
-        response = self.bot.brevity_term_service.get_brevity_terms_by_term(
-            search_term
-        )
-        output_message = ""
-
-        if response:
-            for brevity_term in response:
-                output_message += BrevityTermCog.format_brevity_term(
-                    brevity_term
-                )
-                output_message += "\n"
-        else:
-            output_message = f"No brevity terms found with `{search_term}`"
 
         return output_message
